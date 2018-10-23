381027e92a6ac3d478f6ec9eb0a157ce4a77cfe1
515hikaru <12kojima.takahiro@gmail.com>
2018-10-23 16:41:36 +0900

    64回検査するバージョン

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	var x uint64
-	x = 32
+	x = 1023
 	fmt.Printf("%d\n", popcount.PopCount(x))
+	fmt.Printf("%d\n", popcount.PopCount2(x))
 }
--- popcount.go
+++ popcount.go
@@ -11,12 +11,32 @@ func init() {
 // PopCount は x のポピュレーションカウント(1が設定されているビット数)
 // を数える
 func PopCount(x uint64) int {
-	return int(pc[byte(x>>0*8)] +
-		pc[byte(x>>1*8)] +
-		pc[byte(x>>2*8)] +
-		pc[byte(x>>3*8)] +
-		pc[byte(x>>4*8)] +
-		pc[byte(x>>5*8)] +
-		pc[byte(x>>6*8)] +
-		pc[byte(x>>7*8)])
+	var sum int
+	var i uint64
+	for i = 0; i < 8; i++ {
+		sum += int(pc[byte(x>>(i*8))])
+	}
+	return sum
+}
+
+// 愚直な実装
+func PopCount2(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// 最下位ビットの検査を64回繰り返す方法
+func PopCount3(x uint64) int {
+	var sum int
+	var i uint64
+	for i = 0; i < 64; i++ {
+		sum += int(byte((x >> i) & 1))
+	}
+	return sum
 }
